414310da5f0d4328a09db5516364f2d0f6aaa85d
Farhan <fmshyder@gmail.com>
2019-11-11 18:34:54 -0500

    fixed

--- chittyenough/src/app/app.module.ts
+++ chittyenough/src/app/app.module.ts
@@ -9,13 +9,13 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { FormsModule } from '@angular/forms';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyASAc-fxgMYYP2vXnxwKYQjyRwYTxTcWp4",
-  authDomain: "chittyenough.firebaseapp.com",
-  databaseURL: "https://chittyenough.firebaseio.com",
-  projectId: "chittyenough",
-  storageBucket: "chittyenough.appspot.com",
-  messagingSenderId: "180571597720",
-  appId: "1:180571597720:web:8adabb9a03f1efc3e959ed"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 @NgModule({
