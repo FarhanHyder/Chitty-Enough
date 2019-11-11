dbc51d7185c67feb3a17e3d1df0ca96dcf43de9a
Farhan <fmshyder@gmail.com>
2019-11-11 17:24:39 -0500

    firestore init

--- chittyenough/src/app/app.module.ts
+++ chittyenough/src/app/app.module.ts
@@ -3,12 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { FormsModule } from '@angular/forms';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyASAc-fxgMYYP2vXnxwKYQjyRwYTxTcWp4",
+  authDomain: "chittyenough.firebaseapp.com",
+  databaseURL: "https://chittyenough.firebaseio.com",
+  projectId: "chittyenough",
+  storageBucket: "chittyenough.appspot.com",
+  messagingSenderId: "180571597720",
+  appId: "1:180571597720:web:8adabb9a03f1efc3e959ed"
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig), 
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
